9b40743b877e5b84deb671e2adfdc6b5443ecbd7
Karthikaveni <karthikaveni18@gmail.com>
2017-07-08 12:34:05 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class vowels {
+	public static void main(String args[]){
+		Scanner in =new Scanner(System.in);
+		System.out.println("enter the string");
+		String s=in.nextLine();
+		String res;
+		StringBuffer sb=new StringBuffer(s);
+		sb.reverse();
+		String s1=sb.toString();
+		System.out.println(s1);
+		 res = s1.replaceAll("[aeiouAEIOU]", "");
+		  System.out.println(res);  
+	 
+	}
+	}
